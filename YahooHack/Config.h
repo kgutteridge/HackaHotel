7cfedb0adc2f6f03195d44390b038a80aab015f7
Kieran Gutteridge <kgutteridge@gmail.com>
2013-04-27 16:23:59 +0100

    Adding config file

---
+++ YahooHack/Config.h
@@ -0,0 +1,13 @@
+//
+//  Config.h
+//  YahooHack
+//
+//  Created by Kieran Gutteridge on 27/04/2013.
+//  Copyright (c) 2013 Duchy Software Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Config : NSObject
+
+@end
