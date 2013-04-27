595871f4cad77a610677cc6b2b0c53c95f085cb7
Matt Glover <matt@duchysoftware.com>
2013-04-27 15:27:36 +0100

    Initial Commit

---
+++ YahooHack/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YahooHack
+//
+//  Created by Matt Glover on 27/04/2013.
+//  Copyright (c) 2013 Duchy Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
